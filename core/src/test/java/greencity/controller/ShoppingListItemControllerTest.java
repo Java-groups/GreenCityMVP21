8b00cad96e4d30831103c95debad44a0c68d8716
df7734 <df7734@gmail.com>
2024-05-18 18:52:28 +0300

    [256] code refactoring

--- core/src/test/java/greencity/controller/ShoppingListItemControllerTest.java
+++ core/src/test/java/greencity/controller/ShoppingListItemControllerTest.java
@@ -7,6 +7,7 @@ import greencity.dto.user.UserShoppingListItemResponseDto;
 import greencity.enums.ShoppingListItemStatus;
 import greencity.service.*;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -42,6 +43,12 @@ class ShoppingListItemControllerTest {
 
     static final String shoppingListItemLink = "/user/shopping-list-items";
 
+    static final Long USER_ID = 128L;
+    static final Long USER_SHOPPING_LIST_ITEM_ID = 211L;
+    static final Long HABIT_ID = 45L;
+    static final String LOCALE = Locale.FRENCH.getLanguage();
+    static final String LOCALE_DEFAULT = Locale.ENGLISH.getLanguage();
+
     @BeforeEach
     void setup() {
         this.mockMvc = MockMvcBuilders.standaloneSetup(shoppingListItemController)
@@ -51,10 +58,8 @@ class ShoppingListItemControllerTest {
     }
 
     @Test
+    @DisplayName("Verify the response to a request for bulk deletion of user shopping list items. [Expected: status().isOk()]")
     void bulkDeleteUserShoppingListItemTest() throws Exception{
-        // Verify the response to a request for bulk deletion of user shopping list items.
-        //Expected: status().isOk()
-
         List<Long> deletedItemIds = Arrays.asList(1L, 2L);
         when(shoppingListItemService.deleteUserShoppingListItems("1,2")).thenReturn(deletedItemIds);
 
@@ -71,92 +76,65 @@ class ShoppingListItemControllerTest {
     }
 
     @Test
+    @DisplayName("Verify the response to updating a shopping list item status with a specified language parameter. [Expected: status().isCreated()]")
     void updateUserShoppingListItemStatusWithLanguageParamTest() throws Exception{
-        //Verify the response to updating a shopping list item status with a specified language parameter.
-        //Expected: status().isCreated()
-
-        Locale locale = Locale.FRENCH;
-        Long userShoppingListItemId = 211L;
-        Long userId = 128L;
-
         UserShoppingListItemResponseDto userShoppingListItemResponseDto = UserShoppingListItemResponseDto
-                .builder().id(userId).text("Created").status(ShoppingListItemStatus.INPROGRESS).build();
+                .builder().id(USER_ID).text("Created").status(ShoppingListItemStatus.INPROGRESS).build();
 
-        when(shoppingListItemService.updateUserShopingListItemStatus(userId, userShoppingListItemId, locale.getLanguage()))
+        when(shoppingListItemService.updateUserShopingListItemStatus(USER_ID, USER_SHOPPING_LIST_ITEM_ID, LOCALE))
                 .thenReturn(userShoppingListItemResponseDto);
 
-
-        mockMvc.perform(patch(shoppingListItemLink + "/{userShoppingListItemId}", userShoppingListItemId)
-                        .header("Accept-Language", locale.getLanguage())
-                        .param("id", userId.toString()))
+        mockMvc.perform(patch(shoppingListItemLink + "/{userShoppingListItemId}", USER_SHOPPING_LIST_ITEM_ID)
+                        .header("Accept-Language", LOCALE)
+                        .param("id", USER_ID.toString()))
                         .andExpect(status().isCreated());
 
-        verify(shoppingListItemService).updateUserShopingListItemStatus(userId, userShoppingListItemId, locale.getLanguage());
+        verify(shoppingListItemService).updateUserShopingListItemStatus(USER_ID, USER_SHOPPING_LIST_ITEM_ID, LOCALE);
     }
 
-
     @Test
+    @DisplayName("Verify the response to updating a shopping list item's status. [Expected: status().isOk()]")
     void updateUserShoppingListItemStatus() throws Exception {
-//        Verify the response to updating a shopping list item's status.
-//        Expected: status().isOk()
-
-        Locale locale = Locale.FRENCH;
-        Long userShoppingListItemId = 211L;
-        Long userId = 128L;
         String status = ShoppingListItemStatus.INPROGRESS.name();
 
         UserShoppingListItemResponseDto userShoppingListItemResponseDto = UserShoppingListItemResponseDto
-                .builder().id(userId).text("Created").status(ShoppingListItemStatus.INPROGRESS).build();
+                .builder().id(USER_ID).text("Created").status(ShoppingListItemStatus.INPROGRESS).build();
 
-        when(shoppingListItemService.updateUserShoppingListItemStatus(userId, userShoppingListItemId, locale.getLanguage(), status))
+        when(shoppingListItemService.updateUserShoppingListItemStatus(USER_ID, USER_SHOPPING_LIST_ITEM_ID, LOCALE, status))
                 .thenReturn(List.of(userShoppingListItemResponseDto));
 
-
-        mockMvc.perform(patch(shoppingListItemLink + "/{userShoppingListItemId}/status/{status}", userShoppingListItemId, status)
-                        .header("Accept-Language", locale.getLanguage())
-                        .param("id", userId.toString()))
+        mockMvc.perform(patch(shoppingListItemLink + "/{userShoppingListItemId}/status/{status}", USER_SHOPPING_LIST_ITEM_ID, status)
+                        .header("Accept-Language", LOCALE)
+                        .param("id", USER_ID.toString()))
                 .andExpect(status().isOk());
 
-        verify(shoppingListItemService).updateUserShoppingListItemStatus(userId, userShoppingListItemId, locale.getLanguage(), status);
+        verify(shoppingListItemService).updateUserShoppingListItemStatus(USER_ID, USER_SHOPPING_LIST_ITEM_ID, LOCALE, status);
     }
 
 
     @Test
+    @DisplayName("Verify the response to updating a shopping list item status without specifying a language parameter. [Expected: status().isCreated()]")
     void updateUserShoppingListItemStatusWithoutLanguageParamTest() throws Exception {
-//        Verify the response to updating a shopping list item status without specifying a language parameter.
-//        Expected: status().isCreated()
-
-        String localeDefault = Locale.ENGLISH.getLanguage();
-        Long userShoppingListItemId = 211L;
-        Long userId = 128L;
-
         UserShoppingListItemResponseDto userShoppingListItemResponseDto = UserShoppingListItemResponseDto
-                .builder().id(userId).text("Created").status(ShoppingListItemStatus.INPROGRESS).build();
+                .builder().id(USER_ID).text("Created").status(ShoppingListItemStatus.INPROGRESS).build();
 
-        when(shoppingListItemService.updateUserShopingListItemStatus(userId, userShoppingListItemId, localeDefault))
+        when(shoppingListItemService.updateUserShopingListItemStatus(USER_ID, USER_SHOPPING_LIST_ITEM_ID, LOCALE_DEFAULT))
                 .thenReturn(userShoppingListItemResponseDto);
 
-
-        mockMvc.perform(patch(shoppingListItemLink + "/{userShoppingListItemId}", userShoppingListItemId)
-                        .param("id", userId.toString()))
+        mockMvc.perform(patch(shoppingListItemLink + "/{userShoppingListItemId}", USER_SHOPPING_LIST_ITEM_ID)
+                        .param("id", USER_ID.toString()))
                 .andExpect(status().isCreated());
 
-        verify(shoppingListItemService).updateUserShopingListItemStatus(userId, userShoppingListItemId, localeDefault);
+        verify(shoppingListItemService).updateUserShopingListItemStatus(USER_ID, USER_SHOPPING_LIST_ITEM_ID, LOCALE_DEFAULT);
     }
 
     @Test
+    @DisplayName("Test saving a user's shopping list item without specifying a language parameter. [Expected: status().isCreated()]")
     void saveUserShoppingListItemWithoutLanguageParamTest() throws Exception {
-//        Test saving a user's shopping list item without specifying a language parameter.
-//        Expected: status().isCreated()
-
-        String localeDefault = Locale.ENGLISH.getLanguage();
-        Long habitId = 45L;
-        Long userId = 128L;
-
         ShoppingListItemRequestDto shoppingListItemRequestDto = ShoppingListItemRequestDto.builder().id(1L).build();
         UserShoppingListItemResponseDto userShoppingListItemResponseDto = UserShoppingListItemResponseDto.builder().build();
 
-        when(shoppingListItemService.saveUserShoppingListItems(userId, habitId, List.of(shoppingListItemRequestDto), localeDefault))
+        when(shoppingListItemService.saveUserShoppingListItems(USER_ID, HABIT_ID, List.of(shoppingListItemRequestDto), LOCALE_DEFAULT))
                 .thenReturn(List.of(userShoppingListItemResponseDto));
 
 
@@ -164,94 +142,73 @@ class ShoppingListItemControllerTest {
 
 
         mockMvc.perform(post(shoppingListItemLink)
-                        .header("Accept-Language", localeDefault)
-                        .param("id", userId.toString())
-                        .param("habitId", habitId.toString())
+                        .header("Accept-Language", LOCALE_DEFAULT)
+                        .param("id", USER_ID.toString())
+                        .param("habitId", HABIT_ID.toString())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(requestBody))
                         .andExpect(status().isCreated());
 
-        verify(shoppingListItemService).saveUserShoppingListItems(userId, habitId, List.of(shoppingListItemRequestDto), localeDefault);
+        verify(shoppingListItemService).saveUserShoppingListItems(USER_ID, HABIT_ID, List.of(shoppingListItemRequestDto), LOCALE_DEFAULT);
     }
 
     @Test
+    @DisplayName("Verify the response to a request for user shopping list items with a specified language parameter. [Expected: status().isOk()]")
     void getUserShoppingListItemsWithLanguageParamTest() throws Exception{
-//        Verify the response to a request for user shopping list items with a specified language parameter.
-//        Expected: status().isOk()
-
-        Locale locale = Locale.FRENCH;
-        Long habitId = 45L;
-        Long userId = 128L;
-
         UserShoppingListItemResponseDto userShoppingListItemResponseDto = UserShoppingListItemResponseDto
-                .builder().id(userId).text("Created").status(ShoppingListItemStatus.INPROGRESS).build();
+                .builder().id(USER_ID).text("Created").status(ShoppingListItemStatus.INPROGRESS).build();
 
-        when(shoppingListItemService.getUserShoppingList(userId, habitId, locale.getLanguage()))
+        when(shoppingListItemService.getUserShoppingList(USER_ID, HABIT_ID, LOCALE))
                 .thenReturn(List.of(userShoppingListItemResponseDto));
 
-
-        mockMvc.perform(get(shoppingListItemLink + "/habits/{habitId}/shopping-list", habitId)
-                        .header("Accept-Language", locale.getLanguage())
-                        .param("id", userId.toString()))
+        mockMvc.perform(get(shoppingListItemLink + "/habits/{habitId}/shopping-list", HABIT_ID)
+                        .header("Accept-Language", LOCALE)
+                        .param("id", USER_ID.toString()))
                         .andExpect(status().isOk());
 
-        verify(shoppingListItemService).getUserShoppingList(userId, habitId, locale.getLanguage());
+        verify(shoppingListItemService).getUserShoppingList(USER_ID, HABIT_ID, LOCALE);
     }
 
     @Test
+    @DisplayName("Verify the response to a request for user shopping list items without specifying a language parameter. [Expected: status().isOk()]")
     void getUserShoppingListItemWithoutLanguageParamTest() throws Exception{
-//        Verify the response to a request for user shopping list items without specifying a language parameter.
-//        Expected: status().isOk()
-
-        String localeDefault = Locale.ENGLISH.getLanguage();
-        Long habitId = 45L;
-        Long userId = 128L;
-
         UserShoppingListItemResponseDto userShoppingListItemResponseDto = UserShoppingListItemResponseDto
-                .builder().id(userId).text("Created").status(ShoppingListItemStatus.INPROGRESS).build();
+                .builder().id(USER_ID).text("Created").status(ShoppingListItemStatus.INPROGRESS).build();
 
-        when(shoppingListItemService.getUserShoppingList(userId, habitId, localeDefault))
+        when(shoppingListItemService.getUserShoppingList(USER_ID, HABIT_ID, LOCALE_DEFAULT))
                 .thenReturn(List.of(userShoppingListItemResponseDto));
 
-
-        mockMvc.perform(get(shoppingListItemLink + "/habits/{habitId}/shopping-list", habitId)
-                        .param("id", userId.toString()))
+        mockMvc.perform(get(shoppingListItemLink + "/habits/{habitId}/shopping-list", HABIT_ID)
+                        .param("id", USER_ID.toString()))
                         .andExpect(status().isOk());
 
-        verify(shoppingListItemService).getUserShoppingList(userId, habitId, localeDefault);
+        verify(shoppingListItemService).getUserShoppingList(USER_ID, HABIT_ID, LOCALE_DEFAULT);
     }
 
     @Test
+    @DisplayName("Test deleting a user's shopping list item by item ID, user ID, and habit ID. [Expected: status().isOk()]")
     void deleteTest() throws Exception{
-        //Test deleting a user's shopping list item by item ID, user ID, and habit ID.
-        //Expected: status().isOk()
-
-        mockMvc.perform(delete(shoppingListItemLink + "?id=128")
-                        .param("shoppingListItemId", "4")
-                        .param("habitId", "23"))
+        mockMvc.perform(delete(shoppingListItemLink)
+                        .param("id", USER_ID.toString())
+                        .param("shoppingListItemId", USER_SHOPPING_LIST_ITEM_ID.toString())
+                        .param("habitId", HABIT_ID.toString()))
                         .andExpect(status().isOk());
 
-        verify(shoppingListItemService).deleteUserShoppingListItemByItemIdAndUserIdAndHabitId(4L, 128L, 23L);
+        verify(shoppingListItemService).deleteUserShoppingListItemByItemIdAndUserIdAndHabitId(USER_SHOPPING_LIST_ITEM_ID, USER_ID, HABIT_ID);
     }
 
-
     @Test
+    @DisplayName("Verify the response to a request for all items in progress for a specific user. [Expected: status().isOk()]")
     void findAllByUserTest() throws Exception{
-//        Verify the response to a request for all items in progress for a specific user.
-//        Expected: status().isOk()
+        ShoppingListItemDto shoppingListItemDto = ShoppingListItemDto.builder().id(USER_ID).text(LOCALE).build();
 
-        Long userId = 128L;
-        String locale = Locale.GERMAN.toString();
-
-        ShoppingListItemDto shoppingListItemDto = ShoppingListItemDto.builder().id(userId).text(locale).build();
-
-        when(shoppingListItemService.findInProgressByUserIdAndLanguageCode(userId, locale))
+        when(shoppingListItemService.findInProgressByUserIdAndLanguageCode(USER_ID, LOCALE))
                 .thenReturn(List.of(shoppingListItemDto));
 
-        mockMvc.perform(get(shoppingListItemLink + "/{userId}/get-all-inprogress", userId.toString())
-                        .param("lang", locale))
+        mockMvc.perform(get(shoppingListItemLink + "/{userId}/get-all-inprogress", USER_ID.toString())
+                        .param("lang", LOCALE))
                         .andExpect(status().isOk());
 
-        verify(shoppingListItemService).findInProgressByUserIdAndLanguageCode(userId, locale);
+        verify(shoppingListItemService).findInProgressByUserIdAndLanguageCode(USER_ID, LOCALE);
     }
 }
