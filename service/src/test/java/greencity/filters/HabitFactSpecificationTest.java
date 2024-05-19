2be5db95677d186987adba3928f1f33aa884f247
RockTF <egorov2001egorr@gmail.com>
2024-05-19 08:49:06 +0300

    [268] create unit test to HabitFactSpecificationTest - Added a new HabitFactSpecificationTest class for testing HabitFactSpecification.

--- service/src/test/java/greencity/filters/HabitFactSpecificationTest.java
+++ service/src/test/java/greencity/filters/HabitFactSpecificationTest.java
@@ -29,15 +29,16 @@ class HabitFactSpecificationTest {
 
     @Test
     void toPredicate_IdCriteria_GeneratesCorrectPredicate() {
-        SearchCriteria searchCriteria = new SearchCriteria("id", "=", "1");
+        SearchCriteria searchCriteria = new SearchCriteria("1", "1", "id");
         List<SearchCriteria> criteriaList = List.of(searchCriteria);
         HabitFactSpecification specification = new HabitFactSpecification(criteriaList);
 
-        Path<Long> idPath = mock(Path.class);
+        Path idPath = mock(Path.class);
 
-        when(root.get(HabitFact_.id)).thenReturn(idPath);
+        when(root.get("1")).thenReturn(idPath);
         when(criteriaBuilder.equal(idPath, "1")).thenReturn(mock(Predicate.class));
         when(criteriaBuilder.conjunction()).thenReturn(mock(Predicate.class));
+        when(criteriaBuilder.and(any(Predicate.class), any(Predicate.class))).thenReturn(mock(Predicate.class));
 
         Predicate predicate = specification.toPredicate(root, criteriaQuery, criteriaBuilder);
 
@@ -47,7 +48,7 @@ class HabitFactSpecificationTest {
 
     @Test
     void toPredicate_HabitIdCriteria_GeneratesCorrectPredicate() {
-        SearchCriteria searchCriteria = new SearchCriteria("habitId", "=", "1");
+        SearchCriteria searchCriteria = new SearchCriteria("1", "1", "habitId");
         List<SearchCriteria> criteriaList = List.of(searchCriteria);
         HabitFactSpecification specification = new HabitFactSpecification(criteriaList);
 
@@ -55,6 +56,7 @@ class HabitFactSpecificationTest {
         when(root.join(HabitFact_.habit)).thenReturn(habitJoin);
         when(criteriaBuilder.equal(habitJoin.get(Habit_.id), "1")).thenReturn(mock(Predicate.class));
         when(criteriaBuilder.conjunction()).thenReturn(mock(Predicate.class));
+        when(criteriaBuilder.and(any(Predicate.class), any(Predicate.class))).thenReturn(mock(Predicate.class));
 
         Predicate predicate = specification.toPredicate(root, criteriaQuery, criteriaBuilder);
 
@@ -62,34 +64,34 @@ class HabitFactSpecificationTest {
         verify(criteriaBuilder).equal(habitJoin.get(Habit_.id), "1");
     }
 
+
     @Test
     void toPredicate_ContentCriteria_GeneratesCorrectPredicate() {
-        SearchCriteria searchCriteria = new SearchCriteria("content", "=", "test");
+        SearchCriteria searchCriteria = new SearchCriteria("test", "1", "content");
         List<SearchCriteria> criteriaList = List.of(searchCriteria);
         HabitFactSpecification specification = new HabitFactSpecification(criteriaList);
 
         Root<HabitFactTranslation> habitFactTranslationRoot = mock(Root.class);
-        when(criteriaQuery.from(HabitFactTranslation.class)).thenReturn(habitFactTranslationRoot);
-
-        Predicate contentPredicate = mock(Predicate.class);
-        when(criteriaBuilder.like(habitFactTranslationRoot.get(HabitFactTranslation_.content), "%test%")).thenReturn(contentPredicate);
-
-        Path<Long> habitFactIdPath = mock(Path.class);
         Path<Long> translationHabitFactIdPath = mock(Path.class);
-        when(root.get(HabitFact_.id)).thenReturn(habitFactIdPath);
-        when(habitFactTranslationRoot.get(HabitFactTranslation_.habitFact).get(HabitFact_.id)).thenReturn(translationHabitFactIdPath);
-
-        Predicate idPredicate = mock(Predicate.class);
-        when(criteriaBuilder.equal(translationHabitFactIdPath, habitFactIdPath)).thenReturn(idPredicate);
-
         Predicate conjunctionPredicate = mock(Predicate.class);
+        Predicate contentPredicate = mock(Predicate.class);
+        Path<HabitFact> habitFactPath = mock(Path.class);
+        Path<Long> habitFactIdPath = mock(Path.class);
+        Predicate idPredicate = mock(Predicate.class);
+
+        when(criteriaQuery.from(HabitFactTranslation.class)).thenReturn(habitFactTranslationRoot);
+        when(criteriaBuilder.like(any(Path.class), eq("%test%"))).thenReturn(contentPredicate);
+        when(root.get(HabitFact_.id)).thenReturn(habitFactIdPath);
+        when(habitFactTranslationRoot.get(HabitFactTranslation_.habitFact)).thenReturn(habitFactPath);
+        when(habitFactPath.get(HabitFact_.id)).thenReturn(translationHabitFactIdPath);
+        when(criteriaBuilder.equal(translationHabitFactIdPath, habitFactIdPath)).thenReturn(idPredicate);
         when(criteriaBuilder.conjunction()).thenReturn(conjunctionPredicate);
-        when(criteriaBuilder.and(conjunctionPredicate, contentPredicate, idPredicate)).thenReturn(mock(Predicate.class));
+        when(criteriaBuilder.and(any(Predicate.class), any(Predicate.class))).thenReturn(mock(Predicate.class));
 
         Predicate predicate = specification.toPredicate(root, criteriaQuery, criteriaBuilder);
 
         assertNotNull(predicate);
-        verify(criteriaBuilder).like(habitFactTranslationRoot.get(HabitFactTranslation_.content), "%test%");
+        verify(criteriaBuilder).like(any(Path.class), eq("%test%"));
         verify(criteriaBuilder).equal(translationHabitFactIdPath, habitFactIdPath);
     }
 }
