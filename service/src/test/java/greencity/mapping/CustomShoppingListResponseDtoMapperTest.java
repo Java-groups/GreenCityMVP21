06f0e8899d6092a542302cbb5523135f9d43c1eb
Nataliia Povzhyk <nataliiapovzhyk@gmail.com>
2024-05-20 16:52:25 +0300

    fix for CustomShoppingListResponseDtoMapperTest asserts

--- service/src/test/java/greencity/mapping/CustomShoppingListResponseDtoMapperTest.java
+++ service/src/test/java/greencity/mapping/CustomShoppingListResponseDtoMapperTest.java
@@ -1,5 +1,6 @@
 package greencity.mapping;
 
+import greencity.ModelUtils;
 import greencity.dto.shoppinglistitem.CustomShoppingListItemResponseDto;
 import greencity.entity.CustomShoppingListItem;
 import greencity.enums.ShoppingListItemStatus;
@@ -26,20 +27,13 @@ class CustomShoppingListResponseDtoMapperTest {
     @DisplayName("Test mapping from CustomShoppingListItem to CustomShoppingListItemResponseDto")
     void convert() {
 
-        CustomShoppingListItem customShoppingListItem = CustomShoppingListItem.builder()
-                .id(1L)
-                .text("Buy milk")
-                .status(ShoppingListItemStatus.ACTIVE)
-                .dateCompleted(LocalDateTime.now())
-                .build();
+        CustomShoppingListItem customShoppingListItem = ModelUtils.getCustomShoppingListItem();
+        CustomShoppingListItemResponseDto result = mapper.convert(customShoppingListItem);
 
-        CustomShoppingListItemResponseDto dto = mapper.convert(customShoppingListItem);
-
-        assert dto != null;
-
-        assertEquals(dto.getId(), customShoppingListItem.getId());
-        assertEquals(dto.getText(), customShoppingListItem.getText());
-        assertEquals(dto.getStatus(), customShoppingListItem.getStatus());
+        assert result != null;
+        assertEquals(customShoppingListItem.getId(),result.getId());
+        assertEquals( customShoppingListItem.getText(), result.getText());
+        assertEquals(customShoppingListItem.getStatus(), result.getStatus());
 
 
     }
@@ -70,11 +64,10 @@ class CustomShoppingListResponseDtoMapperTest {
 
 
         assert ! toVerify.contains(null);
-        assertEquals (toVerify.size() , 2);
-        assertEquals(toVerify.get(0).getText(), customShoppingListItem1.getText());
-        assertEquals(toVerify.get(1).getText(), customShoppingListItem2.getText());
-
-        assertEquals(toVerify.get(0).getStatus(), ShoppingListItemStatus.ACTIVE);
-        assertEquals(toVerify.get(1).getStatus(), ShoppingListItemStatus.DISABLED);
+        assertEquals (2, toVerify.size() );
+        assertEquals(customShoppingListItem1.getText(), toVerify.get(0).getText());
+        assertEquals(customShoppingListItem2.getText(), toVerify.get(1).getText());
+        assertEquals(ShoppingListItemStatus.ACTIVE, toVerify.get(0).getStatus());
+        assertEquals(ShoppingListItemStatus.DISABLED, toVerify.get(1).getStatus());
     }
 }
