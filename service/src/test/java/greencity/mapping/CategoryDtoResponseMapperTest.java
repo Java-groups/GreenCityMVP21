7fccfe3eb2adb972d0974c6cadddc881e00ef62c
Serhii Zamorii <sezamua@gmail.com>
2024-10-28 19:14:22 +0200

    CategoryDtoResponseMapper tests were done.

---
+++ service/src/test/java/greencity/mapping/CategoryDtoResponseMapperTest.java
@@ -0,0 +1,36 @@
+package greencity.mapping;
+
+import greencity.dto.category.CategoryDto;
+import greencity.dto.category.CategoryDtoResponse;
+import greencity.entity.Category;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryDtoResponseMapperTest {
+    private CategoryDtoResponseMapper categoryDtoResponseMapper;
+
+    @BeforeEach
+    void setUp() {
+        categoryDtoResponseMapper = new CategoryDtoResponseMapper();
+    }
+
+    @Test
+    void convert_newObject() {
+        assertEquals(new CategoryDtoResponse(), categoryDtoResponseMapper.convert(new Category()));
+    }
+
+    @Test
+    void convert_filledObject() {
+        Category category = Category.builder()
+                .id(101L)
+                .name("Test Name")
+                .build();
+        CategoryDtoResponse expectedCategoryDtoResponse = CategoryDtoResponse.builder()
+                .id(101L)
+                .name("Test Name")
+                .build();
+        assertEquals(expectedCategoryDtoResponse, categoryDtoResponseMapper.convert(category));
+    }
+}
