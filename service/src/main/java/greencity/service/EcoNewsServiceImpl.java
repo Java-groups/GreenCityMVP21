bd8787056fa7c282b4abb50b1a5f0374400eb5ef
maks <bersercer70@gmail.com>
2024-10-31 21:41:09 +0200

    1. [Eco-News, PUT/econews/update] added 403 response code behavior 2. Test method

--- service/src/main/java/greencity/service/EcoNewsServiceImpl.java
+++ service/src/main/java/greencity/service/EcoNewsServiceImpl.java
@@ -18,10 +18,7 @@ import greencity.entity.*;
 import greencity.entity.localization.TagTranslation;
 import greencity.enums.Role;
 import greencity.enums.TagType;
-import greencity.exception.exceptions.BadRequestException;
-import greencity.exception.exceptions.NotFoundException;
-import greencity.exception.exceptions.NotSavedException;
-import greencity.exception.exceptions.UnsupportedSortException;
+import greencity.exception.exceptions.*;
 import greencity.filters.EcoNewsSpecification;
 import greencity.filters.SearchCriteria;
 import greencity.repository.EcoNewsRepo;
@@ -487,8 +484,11 @@ public class EcoNewsServiceImpl implements EcoNewsService {
     @Override
     public EcoNewsGenericDto update(UpdateEcoNewsDto updateEcoNewsDto, MultipartFile image, UserVO user) {
         EcoNews toUpdate = modelMapper.map(findById(updateEcoNewsDto.getId()), EcoNews.class);
-        if (user.getRole() != Role.ROLE_ADMIN && !user.getId().equals(toUpdate.getAuthor().getId())) {
+        if (user.getRole() != Role.ROLE_ADMIN) {
             throw new BadRequestException(ErrorMessage.USER_HAS_NO_PERMISSION);
+        }
+        if(!user.getId().equals(toUpdate.getAuthor().getId())) {
+            throw new UserHasNoPermissionToAccessException(ErrorMessage.USER_HAS_NO_PERMISSION);
         }
         enhanceWithNewData(toUpdate, updateEcoNewsDto, image);
         ecoNewsRepo.save(toUpdate);
