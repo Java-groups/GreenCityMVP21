a45f808e0074fb97269fa258e739efaf6971420d
Denys Porplenko <denys.p@trgint.com>
2024-11-11 21:16:54 +0200

    Added endpoint: friends/user/{user_id} and update table user_friends.

--- service/src/main/java/greencity/service/FriendsServiceImpl.java
+++ service/src/main/java/greencity/service/FriendsServiceImpl.java
@@ -2,6 +2,7 @@ package greencity.service;
 
 import greencity.dto.PageableAdvancedDto;
 import greencity.dto.friends.UserFriendDto;
+import greencity.dto.user.UserManagementDto;
 import greencity.entity.User;
 import greencity.repository.UserRepo;
 import lombok.RequiredArgsConstructor;
@@ -9,7 +10,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
@@ -24,6 +24,13 @@ public class FriendsServiceImpl implements FriendsService {
     }
 
     @Override
+    public List<UserManagementDto> findFriends(Long userId) {
+        return userRepo.getAllUserFriends(userId).stream()
+            .map(item -> modelMapper.map(item, UserManagementDto.class))
+            .toList();
+    }
+
+    @Override
     public PageableAdvancedDto<UserFriendDto> findFriendRequests(Long userId, Pageable page) {
         return getPageableAdvancedDtoOfUserFriendDto(userId, userRepo.getAllUserFriendRequests(userId, page));
     }
@@ -33,7 +40,8 @@ public class FriendsServiceImpl implements FriendsService {
         return getPageableAdvancedDtoOfUserFriendDto(userId, userRepo.getAllUserNotFriendsYet(name, userId, page));
     }
 
-    private PageableAdvancedDto<UserFriendDto> getPageableAdvancedDtoOfUserFriendDto(Long userId, Page<User> friendsPage) {
+    private PageableAdvancedDto<UserFriendDto> getPageableAdvancedDtoOfUserFriendDto(
+        Long userId, Page<User> friendsPage) {
         List<UserFriendDto> friendsList = friendsPage.stream()
                 .map(user -> modelMapper.map(user, UserFriendDto.class))
                 .peek(userFriendDto -> userFriendDto.setMutualFriends(
